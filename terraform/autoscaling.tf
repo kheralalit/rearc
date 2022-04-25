7d4dbbfbac3facbdaefcb664d16fff6b743a1b84
unknown <kheralalit@gmail.com>
2022-04-25 07:26:02 +0530

    changes

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -15,7 +15,9 @@ resource "aws_launch_configuration" "ecs_launch_config" {
 resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
   name                 = "asg"
   vpc_zone_identifier  = [aws_subnet.public[0].id]
-  launch_configuration = aws_launch_configuration.ecs_launch_config.name
+  launch_configuration = aws_launch_configuration.ecs_launch_config.name 
+  target_group_arns    = [aws_alb_target_group.app.arn]
+
 
   desired_capacity          = 1
   min_size                  = 1
